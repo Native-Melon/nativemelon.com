df43ebadb0641ce05e8bbeb3b8d80b7d1fe90dec
Obada Kadri <brobada@gmail.com>
2025-03-02 17:19:35 -0800

    Adds about section

--- src/templates/home-page.jsx
+++ src/templates/home-page.jsx
@@ -6,13 +6,13 @@ import Seo from "../components/seo";
 import { Image } from "react-bootstrap";
 
 import ContactForm from "../components/contactForm";
+import AboutCompany from "../components/aboutCompany";
 
 const HomePageTemplate = ({ data, location }) => {
-  const { company_name, slogans } = data?.prismicHomePage?.data || {};
+  const { company_name, about_intro, about_content, slogans } = data?.prismicHomePage?.data || {};
 
   const serviceList = data?.allPrismicService?.nodes || [];
   const portfolioList = data?.allPrismicPortfolioItem?.nodes || [];
-  portfolioList.map(p => console.log(p));
   const slogan = slogans.filter((s) => s.slogan?.text)[
     Math.floor(Math.random() * slogans.length)
   ];
@@ -92,167 +92,8 @@ const HomePageTemplate = ({ data, location }) => {
       </section>
       {/* About */}
       <section className="page-section" id="about">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">About</h2>
-            <h3 className="section-subheading text-muted">
-              Lorem ipsum dolor sit amet consectetur.
-            </h3>
-          </div>
-          <ul className="timeline">
-            <li>
-              <div className="timeline-image">
-                <img
-                  className="rounded-circle img-fluid"
-                  src="assets/img/about/1.jpg"
-                  alt="..."
-                />
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>2009-2011</h4>
-                  <h4 className="subheading">Our Humble Beginnings</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Sunt ut voluptatum eius sapiente, totam reiciendis
-                    temporibus qui quibusdam, recusandae sit vero unde, sed,
-                    incidunt et ea quo dolore laudantium consectetur!
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li className="timeline-inverted">
-              <div className="timeline-image">
-                <img
-                  className="rounded-circle img-fluid"
-                  src="assets/img/about/2.jpg"
-                  alt="..."
-                />
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>March 2011</h4>
-                  <h4 className="subheading">An Agency is Born</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Sunt ut voluptatum eius sapiente, totam reiciendis
-                    temporibus qui quibusdam, recusandae sit vero unde, sed,
-                    incidunt et ea quo dolore laudantium consectetur!
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="timeline-image">
-                <img
-                  className="rounded-circle img-fluid"
-                  src="assets/img/about/3.jpg"
-                  alt="..."
-                />
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>December 2015</h4>
-                  <h4 className="subheading">Transition to Full Service</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Sunt ut voluptatum eius sapiente, totam reiciendis
-                    temporibus qui quibusdam, recusandae sit vero unde, sed,
-                    incidunt et ea quo dolore laudantium consectetur!
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li className="timeline-inverted">
-              <div className="timeline-image">
-                <img
-                  className="rounded-circle img-fluid"
-                  src="assets/img/about/4.jpg"
-                  alt="..."
-                />
-              </div>
-              <div className="timeline-panel">
-                <div className="timeline-heading">
-                  <h4>July 2020</h4>
-                  <h4 className="subheading">Phase Two Expansion</h4>
-                </div>
-                <div className="timeline-body">
-                  <p className="text-muted">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Sunt ut voluptatum eius sapiente, totam reiciendis
-                    temporibus qui quibusdam, recusandae sit vero unde, sed,
-                    incidunt et ea quo dolore laudantium consectetur!
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li className="timeline-inverted">
-              <div className="timeline-image">
-                <h4>
-                  Be Part
-                  <br />
-                  Of Our
-                  <br />
-                  Story!
-                </h4>
-              </div>
-            </li>
-          </ul>
-        </div>
+        <AboutCompany intro={about_intro} content={about_content} />
       </section>
-      {/* Clients */}
-      <div className="py-5">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-md-3 col-sm-6 my-3">
-              <a href="#!">
-                <img
-                  className="img-fluid img-brand d-block mx-auto"
-                  src="assets/img/logos/microsoft.svg"
-                  alt="..."
-                  aria-label="Microsoft Logo"
-                />
-              </a>
-            </div>
-            <div className="col-md-3 col-sm-6 my-3">
-              <a href="#!">
-                <img
-                  className="img-fluid img-brand d-block mx-auto"
-                  src="assets/img/logos/google.svg"
-                  alt="..."
-                  aria-label="Google Logo"
-                />
-              </a>
-            </div>
-            <div className="col-md-3 col-sm-6 my-3">
-              <a href="#!">
-                <img
-                  className="img-fluid img-brand d-block mx-auto"
-                  src="assets/img/logos/facebook.svg"
-                  alt="..."
-                  aria-label="Facebook Logo"
-                />
-              </a>
-            </div>
-            <div className="col-md-3 col-sm-6 my-3">
-              <a href="#!">
-                <img
-                  className="img-fluid img-brand d-block mx-auto"
-                  src="assets/img/logos/ibm.svg"
-                  alt="..."
-                  aria-label="IBM Logo"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
       {/* Contact */}
       <section className="page-section" id="contact">
         <ContactForm />
@@ -278,6 +119,12 @@ export const homePageQuery = graphql`
       data {
         company_name {
           text
+        }
+        about_intro {
+          text
+        }
+        about_content {
+          html
         }
         slogans {
           slogan {
